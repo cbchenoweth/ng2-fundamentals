567071887f90ed9b377c78bf78eb2d3828d64baf
calebc <caleb.chenoweth@dat.com>
2018-03-27 13:06:09 -0700

    route back to events page after login / cancel login.

--- app/users/login.component.ts
+++ app/users/login.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from "@angular/core";
 import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
 
 @Component({
     templateUrl: 'app/users/login.component.html'
 })
 export class LoginComponent implements OnInit {
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService, private router: Router) {
 
     }
 
@@ -15,5 +16,14 @@ export class LoginComponent implements OnInit {
 
     login(formValues) {
         this.authService.loginUser(formValues.userName, formValues.password);
+        this.returnToEventsPage();
+    }
+
+    cancel() {
+        this.returnToEventsPage();
+    }
+
+    private returnToEventsPage() {
+        this.router.navigate(['events']);
     }
 }
