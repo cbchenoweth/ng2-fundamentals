817e66beac8339f215ee30610f2a151bdeb26a4b
calebc <caleb.chenoweth@dat.com>
2018-03-27 15:44:12 -0700

    add error styling when fields invalid.

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -3,7 +3,14 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {ISession} from "../shared";
 
 @Component({
-    templateUrl: 'app/events/event-details/create-session.component.html'
+    templateUrl: 'app/events/event-details/create-session.component.html',
+    styles: [
+        `
+            em {float: right; color: #E05C65; padding-left: 10px;}
+            .error input, .error select, .error textarea {background-color: #E3C3C5;}
+            .error ::-webkit-input-placeholder {color: #999;}
+        `
+    ]
 })
 export class CreateSessionComponent implements OnInit {
     name: FormControl;
