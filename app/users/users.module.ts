3d848ece74dcb7940b0271d3edd698b827623d12
calebc <caleb.chenoweth@dat.com>
2018-03-27 12:11:28 -0700

    login form captures the entered values and send to component on submit.

--- app/users/users.module.ts
+++ app/users/users.module.ts
@@ -3,10 +3,12 @@ import {CommonModule} from "@angular/common";
 import {UserProfileComponent} from "./user-profile.component";
 import {UsersRoutingModule} from "./users-routing.module";
 import {LoginComponent} from "./login.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         UsersRoutingModule
     ],
     declarations: [
