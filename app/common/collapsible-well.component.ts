795350d047631c9ab6fbc73e206137d3ed5a142b
calebc <caleb.chenoweth@dat.com>
2018-03-27 17:29:20 -0700

    make the collapsible well able to contain complex title html.

--- app/common/collapsible-well.component.ts
+++ app/common/collapsible-well.component.ts
@@ -1,16 +1,17 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
     selector: 'collapsible-well',
     template: `
         <div (click)="toggleContentVisible()" class="well pointable">
-            <h4 class="well-title">{{title}}</h4>
-            <ng-content *ngIf="contentVisible"></ng-content>
+            <h4>
+                <ng-content select="[collapsible-well-title]"></ng-content>
+            </h4>
+            <ng-content *ngIf="contentVisible" select="[collapsible-well-body]"></ng-content>
         </div>
     `
 })
 export class CollapsibleWellComponent implements OnInit {
-    @Input() title: string;
     contentVisible = true;
 
     constructor() {
