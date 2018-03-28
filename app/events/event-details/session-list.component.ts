fe13a2a7248cad2cb0fed1ac217900b0ca484c1b
calebc <caleb.chenoweth@dat.com>
2018-03-28 10:09:36 -0700

    add filtering logic to the session list.

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,12 +1,14 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from "@angular/core";
 import {ISession} from "../shared/index";
 
 @Component({
     selector: 'session-list',
     templateUrl: 'app/events/event-details/session-list.component.html'
 })
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnInit, OnChanges {
     @Input() sessions: ISession[];
+    @Input() filterBy: string;
+    visibleSessions: ISession[] = [];
 
     constructor() {
 
@@ -15,4 +17,17 @@ export class SessionListComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    ngOnChanges(changes: SimpleChanges): void {
+        if(this.sessions) {
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    private filterSessions(filterBy: string) {
+        if(filterBy === 'all') {
+            this.visibleSessions = this.sessions.slice();
+        } else {
+            this.visibleSessions = this.sessions.filter(s => s.level.toLowerCase() === filterBy);
+        }
+    }
 }
