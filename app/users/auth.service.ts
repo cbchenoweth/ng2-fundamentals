4f215305c9f7bbf3a0f4027abdad1e35d5d1a2d2
calebc <caleb.chenoweth@dat.com>
2018-03-29 11:08:28 -0700

    app check server for auth to "auto login" on page refresh.

--- app/users/auth.service.ts
+++ app/users/auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {IUser} from "./user.model";
 import {Headers, Http, RequestOptions, Response} from "@angular/http";
 import {Observable} from "rxjs/Rx";
+import {el} from "@angular/platform-browser/testing/src/browser_util";
 
 @Injectable()
 export class AuthService {
@@ -18,7 +19,7 @@ export class AuthService {
         const loginCredentials = {username: username, password: password};
         return this.http.post(`/api/login`, loginCredentials, options)
             .do(response => {
-                if(response) {
+                if (response) {
                     this.currentUser = <IUser> response.json().user;
                 }
             })
@@ -34,5 +35,22 @@ export class AuthService {
 
         currentUser.firstName = firstName;
         currentUser.lastName = lastName;
+    }
+
+    checkAuthenticationStatus() {
+        this.http.get('/api/currentIdentity')
+            .map((response: any) => {
+                if (response._body) {
+                    return response.json();
+                }
+                else {
+                    return null;
+                }
+            })
+            .subscribe(user => {
+                if (user) {
+                    this.currentUser = user;
+                }
+            });
     }
 }
